5b4e4130a45eba6fe0988cdbc05de7fd1ad05178
Steven Torrence <storrence88@gmail.com>
2020-11-30 21:15:20 -0600

    Remove render blocking CSS fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   DocumentContext
 } from 'next/document';
+import GoogleFonts from 'next-google-fonts';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -15,12 +16,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
+        <GoogleFonts href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap' />
         <Head>
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com/'
+            crossOrigin=''
+          />
+          {/* <link
             href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap'
             rel='stylesheet'
-          />
+          /> */}
         </Head>
         <body>
           <Main />
